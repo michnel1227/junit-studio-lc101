4fbdf9d5c0ff473bededd63666e32aa7ac70c8d6
Michelle <michellenelson36@yahoo.com>
2021-02-02 16:53:15 -0600

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,63 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void beginningBracketSetReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]michelle"));
+    }
+
+    @Test
+    public void splitBracketSetReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[michelle]"));
+    }
+
+    @Test
+    public void mixedLettersAndBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[mich]elle"));
+    }
+
+    @Test
+    public void extraBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[mich]elle]"));
+    }
+
+    @Test
+    public void doubleBracketSetReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[mich]el[le]"));
+    }
+
+    @Test
+    public void nestedBracketSetReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[mich[ell]e]"));
+    }
+
+    @Test
+    public void missingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void sameSideBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void oneCurlyBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{]"));
+    }
+
+    @Test
+    public void reversedBracketWithWordsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]michelle["));
+    }
+
+    @Test
+    public void reverseBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 }
